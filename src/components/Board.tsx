40fa0a86a7e47d6b1d0fb5ccf2f59d51f097a1ca
Miguel Domingos <miguel.domingos@ctw.bmwgroup.com>
2024-08-02 14:36:44 +0100

    Editable title and bordered cards

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,34 +5,54 @@ export function Board() {
     const [columns, setColumns] = useState<Column[]>([
         {title: "Column 1", cards: [{text: "Card 1"}, {text: "Card 2"}]},
         {title: "Column 2", cards: [{text: "Card 1"}]},
-        {title: "Column 3", cards: [{text: "Card 1"},{text: "Card 2"},{text: "Card 3"}]},
+        {title: "Column 3", cards: [{text: "Card 1"}, {text: "Card 2"}, {text: "Card 3"}]},
     ])
+
+    const addColumns = () => {
+        setColumns([...columns, {title: `Column ${columns.length + 1}`, cards: [{text: "Card 1"}]},])
+    }
+
+    const setTitle = (title: string, index: number) => {
+        const newColumns = [...columns]
+        newColumns[index].title = title
+        setColumns(newColumns)
+    }
 
     return (
         <>
             <div className={"container"}>
-                {
-                    columns.map((column: Column, index) => (
-                        <div className={`column`}
-                             key={index}
-                             style={{backgroundColor: "grey"}}
-                        >
-                            {column.title}
-                            {
-                                column.cards.map((card: Card, index) => (
-                                    <div className={`card`}
-                                         key={index}
-                                         style={{backgroundColor: "lightgrey"}}
-                                    >
-                                        {card.text}
-                                    </div>
-                                ))
+                <div className={"columns-container"}>
+                    {
+                        columns.map((column: Column, index) => (
+                            <div className={`column`}
+                                 key={index}
+                            >
+                                    <input
+                                        type="text"
+                                        value={column.title}
+                                        onChange={
+                                            event => setTitle(event.target.value, index)
+                                        }
+                                    />
 
-                            }
-                        </div>
-                    ))
-                }
+                                {
+                                    column.cards.map((card: Card, index) => (
+                                        <div className={`card`}
+                                             key={index}
+                                        >
+                                            {card.text}
+                                        </div>
+                                    ))
+
+                                }
+                            </div>
+                        ))
+                    }
+                </div>
+                <button className={"add-column-button"} onClick={addColumns}>Add column</button>
             </div>
+
+
         </>
     )
 }
